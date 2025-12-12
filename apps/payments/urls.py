c12ab7bc164c8bc09bf499be4ba600e45995a1c6
Abdulhodiyev <armiyatv05@gmail.com>
2025-12-12 22:08:28 +0500

    payments done

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -1,4 +1,4 @@
-# core/urls.py (yoki project router)
+# core/urls.py
 from django.http import HttpResponse
 from django.urls import path, include
 from apps.payments.views import click_webhook, payment_webhook
@@ -9,9 +9,13 @@ router = DefaultRouter()
 router.register(r'payments', PaymentViewSet, basename='payments')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    # Webhooks MUST be ABOVE router
     path('api/payments/webhook/', payment_webhook, name='payment-webhook'),
     path('api/payments/click-webhook/', click_webhook, name='click-webhook'),
-    # optional: fake pay page for manual simulation
+
+    # Optional fake pay page
     path('api/payments/fake-pay/<int:payment_id>/', lambda r, payment_id: HttpResponse("fake pay page")),
+
+    # Router comes LAST
+    path('api/', include(router.urls)),
 ]
